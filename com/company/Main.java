2285cf0f0c6caf7e41075559f64da9672700a90f
Allen St Fort <allenstfort1@gmail.com>
2021-12-22 13:39:13 -0500

    Arrays, Multi-Dimensional Arrays, & Constants

--- com/company/Main.java
+++ com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.awt.*;
+import java.util.Arrays;
 
 
 public class Main {
@@ -12,8 +13,26 @@ public class Main {
         point1.x = 2;
         String message = " Hello World" + "!! ";
         message.endsWith("!!");
-        System.out.println(message.trim());
-        System.out.println(message);
+        // Arrays
+        // To set up arrays
+        int[] setOfNumbers1 = new int[5];
+        // To put values into the array that is declared.
+        setOfNumbers1[0] = 1;
+        setOfNumbers1[1] = 2;
+
+        // Current way arrays are set up.
+        int[] setOfNumbers2 = {3,4,5,6};
+
+        // To set up a Multi-dimensional array
+        int [][] setOfNumber3 = {{1,2,3}, {4,5,6}};
+
+        // Constants
+        final float pi = 3.14F;
+
+        System.out.println("This is a Multi-dimensional array: " + Arrays.deepToString(setOfNumber3));
+        System.out.println("This is the length of the array2: " + setOfNumbers2.length);
+
+        System.out.println(Arrays.toString(setOfNumbers2));
 
     }
 }
